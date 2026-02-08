73dafa482c2a26d9011f1d0b62ff341811767b74
Marsu2 <elouanbarbier296@gmail.com>
2026-02-08 14:37:02 +0100

    feat: Creation server

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+
+
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+    res.send('Le serveur fonctionne correctement')
+})
+
+http.listen(3000, () => {
+    console.log('Serveur lancé sur http://localhost:3000')
+})
+
+io.on('connection', (socket) => {
+    console.log('Un utilisateur s\'est connecté')
+
+    socket.on('message', (msg) => {
+        io.emit('message', msg)
+        console.log(msg)
+    })
+})
