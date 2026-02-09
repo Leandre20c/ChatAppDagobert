0ffe6e1c3f835649efbac0f9bdc838cb0d4bdf1a
Leandre <leandre.vincent@epita.fr>
2026-02-09 16:25:42 +0100

    trying to implement rooms

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 const rooms = []
 
+// State
+const UsersState = {
+    users: [],
+    setUsers: function (newUsersArray) {
+        this.user = newUsersArray
+    }
+}
+
 app.use('/', express.static('public', { index: 'connexion.html' }));
 
 app.get('/', (req, res) => {
@@ -15,7 +23,8 @@ http.listen(3000, () => {
 })
 
 io.on('connection', (socket) => {
-    console.log('Un utilisateur s\'est connecté')
+    const currUser = socket.id
+    console.log('User ' + currUser + ' connected :)')
 
     socket.on('message', (msg) => {
         io.emit('message', msg)
@@ -25,20 +34,102 @@ io.on('connection', (socket) => {
     socket.on('create-room', (roomName) => {
         if (roomName != null) {
             rooms.push(roomName)
-            socket.join(roomName)
-            socket.currentRoom = roomName
-            socket.emit('message', "Vous avez créé et rejoint la room : "+roomName)
+            joinRoom(roomName)
+            socket.emit('message', "Vous avez créé : "+roomName)
             console.log("room " +roomName+" was created and joined by" +socket.id)
             console.log(rooms)
         }
     })
 
-    socket.on('join-room', (roomName) => {
+    socket.on('join-room', ({username, roomName}) => {
         if (roomName != null && rooms.indexOf(roomName) !== -1) {
-            socket.join(roomName)
-            socket.currentRoom = roomName
-            socket.emit('message', "Vous avez rejoint la room : "+roomName)
-            console.log("room " +roomName+" was joined")
+            const prevRoom = getUser(currUser)?.room
+
+            if (prevRoom != null) {
+                socket.leave(prevRoom)
+                io.to(prevRoom).emit('message', buildMessage('INFO', username + ' has left the room'))
+            }
+
+            const user = activateUser(currUser, name, room)
+
+            if (prevRoom) {
+                io.to(prevRoom).emit('userList', {
+                    users: [getUsersInRoom(prevRoom)]
+                })
+            }
+
+            socket.join(user.room)
+
+            socket.emit('message', buildMessage("INFO", "You joined the room: " + user.room))
+
+            io.to(user.room).emit('userList', {
+                users: getUsersInRoom(user.room)
+            })
+
+            io.emit('roomList', {
+                rooms: getAllActiveRooms()
+            })
         }
     })
+
+    // When user disconnect
+    socket.on('disconnect', () => {
+        const user = getUser(socket.id)
+        userLeavesApp(socket.id)
+
+        if (user) {
+            io.to(user.room).emit('message', buildMsg(ADMIN, `${user.name} has left the room`))
+
+            io.to(user.room).emit('userList', {
+                users: getUsersInRoom(user.room)
+            })
+
+            io.emit('roomList', {
+                rooms: getAllActiveRooms()
+            })
+        }
+
+        console.log(`User ${socket.id} disconnected`)
+    })
 })
+
+
+function buildMessage(name, text) {
+    return {
+        name,
+        text,
+        time: new Intl.DateTimeFormat('default', {
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric'
+        }).format(new Date())
+    }
+}
+
+// User functions
+function activateUser(id, name, room) {
+    const user = {id, name, room}
+    UsersState.setUsers([
+        ...UsersState.users.filter(user => user.id != id),
+        user
+    ])
+    return user
+}
+
+function userLeavesApp(id) {
+    UsersState.setUsers(
+        UsersState.users.filter(user => user.id !== id)
+    )
+}
+
+function getUser(id) {
+    return UsersState.users.find(user => user.id === id)
+}
+
+function getUsersInRoom(room) {
+    return UsersState.users.filter(user => user.room === room)
+}
+
+function getAllActiveRooms() {
+    return Array.from(new Set(UsersState.users.map(user => user.room)))
+}
--- public/script.js
+++ public/script.js
@@ -1,30 +1,44 @@
 const socket = io()
 
-const message_form = document.getElementById('message-form')
 const message_input = document.getElementById('message-input')
-
-const messages = document.getElementById('display-messages')
-
+const chatDisplay = document.querySelector('.chat-display')
 const disconnect_button = document.getElementById("disconnect-button")
-
-const create_room_form = document.getElementById('room-create-form')
-const join_room_form = document.getElementById('room-join-form')
-const created_room = document.getElementById('room-create-input')
 const joined_room = document.getElementById('room-join-input')
+const usersList = document.querySelector('.user-list')
+const roomList = document.querySelector('.room-list')
 
 var username = localStorage.getItem("username")
 if (username == null || username == "") {
     window.location.href = 'connexion.html'
 }
 
-message_form.addEventListener('submit', (e) => {
+function sendMessage(e) {
     e.preventDefault()
+    socket.emit('message', {
+        name: username,
+        text: message_input.value
+    })
+    // Clear the text and focus on text entry so user don't have to click on
+    message_input.value = ''
+    message_input.focus()
+}
 
-    if (message_input.value.trim() !== '') {
-        socket.emit('message', "["+username+"] " + message_input.value)
-        message_input.value = ''
-    }
-})
+function enterRoom(e) {
+    e.preventDefault()
+    socket.emit('enterRoom', {
+        name: username,
+        room: joined_room.value
+    })
+
+}
+
+// When join room is pressed
+document.querySelector('.form-join-room')
+    .addEventListener('submit', enterRoom)
+
+// When send message is pressed
+document.querySelector('message-form')
+    .addEventListener('submit', sendMessage)
 
 
 disconnect_button.addEventListener("click", (e) => {
@@ -34,25 +48,46 @@ disconnect_button.addEventListener("click", (e) => {
     window.location.href = 'connexion.html'
 })
 
-socket.on('message', (msg) => {
-    const item = document.createElement('div')
-    item.className = username+"-class"
-    item.textContent = msg
-    messages.appendChild(item)
+socket.on('message', (data) => {
+    const { name, text, time } = data
+    const li = document.createElement('li')
+    li.className = 'post'
+    if (name === username) li.className = 'post post--left'
+    if (name !== username) li.className = 'post post--right'
+    chatDisplay.appendChild(li)
+    chatDisplay.scrollTop = chatDisplay.scrollHeight
 })
 
-create_room_form.addEventListener('submit', (e) => {
-    e.preventDefault()
+socket.On('userList', ({ users }) => {
+    showUsers(users)
+})
 
-    if (created_room.value.trim() !== '') {
-        socket.emit('create-room', created_room.value.trim())
+socket.On('roomList', ({ rooms }) => {
+    showUsers(rooms)
+})
+
+function showUsers(users) {
+    usersList.textContent = ''
+    if (users) {
+        usersList.innerHTML = `<em>Users in ${chatRoom.value}:</em>`
+        users.forEach((user, i) => {
+            usersList.textContent += ` ${user.name}`
+            if (users.length > 1 && i !== users.length - 1) {
+                usersList.textContent += ","
+            }
+        })
     }
-})
+}
 
-join_room_form.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    if (joined_room.value.trim() !== '') {
-        socket.emit('join-room', joined_room.value.trim())
+function showRooms(rooms) {
+    roomList.textContent = ''
+    if (rooms) {
+        roomList.innerHTML = '<em>Active Rooms:</em>'
+        rooms.forEach((room, i) => {
+            roomList.textContent += ` ${room}`
+            if (rooms.length > 1 && i !== rooms.length - 1) {
+                roomList.textContent += ","
+            }
+        })
     }
-})
+}
